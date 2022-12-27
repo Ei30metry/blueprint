b8dc1e2fc3699bf86575cee52f5c12bee0cfd53c
Artin <95337891+Ei30metry@users.noreply.github.com>
2022-12-27 18:04:29 +0330

    Moving away from GHC to haskell-src-exts

--- src/Compute.hs
+++ src/Compute.hs
@@ -1,14 +1,18 @@
 module Compute where
 
-import           Control.Monad.Except (ExceptT (..))
-import Control.Monad.Trans.Class ()
+import           Control.Monad.Except              ( ExceptT (..) )
 import           Control.Monad.IO.Class            ( liftIO )
+import           Control.Monad.ST
+import           Control.Monad.Trans.Class         ()
 
 import           Data.Foldable                     ( find )
+import           Data.IORef                        ( IORef (..), modifyIORef,
+                                                     newIORef, readIORef,
+                                                     writeIORef )
 import           Data.Traversable                  ( traverse )
 
 import           GHC                               ( DynFlags (..),
-                                                     GenLocated (..), Ghc,
+                                                     GenLocated (..),
                                                      GhcMonad (..),
                                                      HsModule (..),
                                                      LoadHowMuch (..),
@@ -18,12 +22,15 @@ import           GHC                               ( DynFlags (..),
                                                      getSessionDynFlags,
                                                      guessTarget, load,
                                                      mgModSummaries,
-                                                     parseModule,
+                                                     parseModule, runGhc,
                                                      setSessionDynFlags )
+import           GHC.Driver.Monad                  ( Ghc (..), Session (..) )
 import           GHC.Driver.Ppr                    ( showSDoc )
+import           GHC.Generics
 import           GHC.Hs.Extension                  ( GhcPs (..) )
 import           GHC.Parser.Annotation             ( SrcSpanAnnA )
 import           GHC.Runtime.Loader
+import           GHC.Types.Error                   ( SDoc (..) )
 import           GHC.Types.Target                  ( Target (..) )
 import           GHC.Unit.Module.ModSummary        ( msHsFilePath )
 import           GHC.Utils.Outputable              ( ppr )
@@ -32,10 +39,12 @@ import           GHC.Utils.Panic                   ( panic )
 import           Language.Haskell.Syntax.Decls     ( HsDecl, LHsDecl )
 import           Language.Haskell.Syntax.Extension ( UnXRec (..), XRec (..) )
 
+import           Unsafe.Coerce                     ( unsafeCoerce )
 
 -- toAST :: GhcMonad m => FilePath -> m [HsDecl GhcPs]
-toAST :: GhcMonad m => FilePath -> m [HsDecl GhcPs]
-toAST fn = do
+-- toHsDeclList :: GhcMonad m => FilePath -> m [HsDecl GhcPs]
+toHsDeclList :: FilePath -> Ghc [HsDecl GhcPs]
+toHsDeclList fn = do
   target <- guessTarget fn Nothing
   addTarget target
   dflags <- getSessionDynFlags

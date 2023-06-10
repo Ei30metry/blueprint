cafc35c5d6a7d26e22194c21ab26069c104b86c4
Artin Ghasivand <ghasivand.artin@gmail.com>
2023-06-10 15:17:28 +0200

    Refactored the Blueprint module

--- blueprint-cli/src/CLI.hs
+++ blueprint-cli/src/CLI.hs
@@ -1,4 +1,13 @@
 -- module CLI (parseSearchEnv) where
-module CLI ( module CLI.Parser ) where
+module CLI ( commandLineInterface ) where
 
 import           CLI.Parser
+import Development.Blueprint.Types
+import Options.Applicative
+
+
+commandLineInterface :: IO SearchEnv
+commandLineInterface = do
+  execParser (info (parseSearchEnv <**> helper)
+                          (fullDesc <> progDesc "Print recursive declerations of an entity"
+                           <> header "A different approach to showing outgoing call hierarchy for Haskell source code."))
--- blueprint-core/src/Development/Blueprint.hs
+++ blueprint-core/src/Development/Blueprint.hs
@@ -1,53 +1,60 @@
 {-
 This module is the frontend to all the functionalities provided by blueprint and
-should be the only module needed in order to make a GUI client for Blueprint
+should be sufficient for most use cases
 -}
 
 module Development.Blueprint where
 
-import           Control.Monad.Except                    ( MonadError (..) )
-import           Control.Monad.Reader                    ( MonadIO (liftIO),
-                                                           MonadReader,
-                                                           (<=<), ask )
+import           Control.Monad.Except              ( MonadError (..) )
+import           Control.Monad.Reader              ( MonadIO (liftIO),
+                                                     MonadReader, ask, (<=<) )
 
-import           Development.Blueprint.Compute.AST       ( tcModuleToTcGblEnv )
-import           Development.Blueprint.Error             ( PipelineError (..) )
+import           Development.Blueprint.Compute.AST ( tcModuleToTcGblEnv )
+import           Development.Blueprint.Error       ( PipelineError (..) )
 
-import           GHC                                     ( Backend (..),
-                                                           HsGroup (hs_annds, hs_valds),
-                                                           LoadHowMuch (..),
-                                                           ModSummary (..),
-                                                           ParsedModule (..),
-                                                           backend,
-                                                           getModSummary,
-                                                           getSessionDynFlags,
-                                                           guessTarget,
-                                                           hs_valds, load,
-                                                           mkModuleName,
-                                                           setSessionDynFlags,
-                                                           setTargets,
-                                                           typecheckModule, Module, LHsBinds, GhcTc, TyCon, ClsInst, FamInst, GhcRn, RealSrcLoc, RealSrcSpan, LImportDecl )
-import           GHC.Driver.Monad                        ( GhcMonad (..) )
-import           GHC.Driver.Session                      ( DynFlags (backend, ghcLink, ghcMode),
-                                                           GhcLink (LinkInMemory),
-                                                           GhcMode (CompManager) )
-import           GHC.Tc.Types                            ( TcGblEnv (..) )
-import           GHC.Types.Name.Reader                   ( GlobalRdrEnv )
-import           GHC.Types.Name.Set                      ( DefUses )
+import           GHC                               ( Backend (..), ClsInst,
+                                                     DynFlags (backend, ghcLink, ghcMode),
+                                                     FamInst,
+                                                     GhcLink (LinkInMemory),
+                                                     GhcMode (CompManager),
+                                                     GhcPs, GhcRn, GhcTc,
+                                                     HsExpr (HsUnboundVar, HsVar),
+                                                     HsGroup (hs_annds, hs_valds),
+                                                     LHsBinds, LHsExpr,
+                                                     LImportDecl,
+                                                     LoadHowMuch (..),
+                                                     ModSummary (..), Module,
+                                                     Name, ParsedModule (..),
+                                                     RealSrcLoc, RealSrcSpan,
+                                                     TyCon, backend,
+                                                     getModSummary,
+                                                     getSessionDynFlags,
+                                                     guessTarget, hs_valds,
+                                                     load, mkModuleName,
+                                                     setSessionDynFlags,
+                                                     setTargets,
+                                                     typecheckModule, HsRecField' (HsRecField) )
+import           GHC.Core.FamInstEnv               ( FamInstEnv )
+import           GHC.Core.InstEnv                  ( InstEnv )
+import           GHC.Core.PatSyn                   ( PatSyn )
+import           GHC.Driver.Monad                  ( GhcMonad (..) )
+import           GHC.Plugins                       ( AnnEnv, Annotation )
+import           GHC.Tc.Types                      ( TcGblEnv (..) )
+import           GHC.Types.Name.Reader             ( GlobalRdrEnv )
+import           GHC.Types.Name.Set                ( DefUses )
+import           GHC.Types.TypeEnv                 ( TypeEnv )
 
-import           HIE.Bios                                ( CradleLoadResult (CradleSuccess),
-                                                           findCradle,
-                                                           getCompilerOptions,
-                                                           loadCradle )
-import           HIE.Bios.Environment                    ( initSession )
+import           HIE.Bios                          ( CradleLoadResult (CradleSuccess),
+                                                     findCradle,
+                                                     getCompilerOptions,
+                                                     loadCradle )
+import           HIE.Bios.Environment              ( initSession )
 
+import           Language.Haskell.Syntax.Expr
+import           Language.Haskell.Syntax.Decls
+import           Language.Haskell.Syntax.Binds
 
-import           System.Directory                        ( getCurrentDirectory )
-import GHC.Types.TypeEnv (TypeEnv)
-import GHC.Core.InstEnv (InstEnv)
-import GHC.Plugins (AnnEnv, Annotation)
-import GHC.Core.PatSyn (PatSyn)
-import GHC.Core.FamInstEnv (FamInstEnv)
+import           System.Directory                  ( getCurrentDirectory )
 
 
 -- | Tries to find the module name by looking for the 'module' keyword
@@ -58,15 +65,12 @@ findModuleName = nonMainModule . dropWhile (/= "module") . words
         nonMainModule _       = throwError CouldntFindModName
 
 
-
 -- | A variant of findModuleName which will assume a source without declare module name is the Main module
 findModuleName' :: (MonadError PipelineError m) => String -> m String
 findModuleName' = nonMainModule . dropWhile (/= "module") . words
   where nonMainModule []      = throwError EmptySrcFile
         nonMainModule (_:x:_) = return x
         nonMainModule _       = return "Main"
-
-
 
 
 -- | Sets up the right environment for ghc to start doing its job
@@ -105,6 +109,7 @@ instance (GhcMonad m, MonadReader a m) => GhcReader a m
 seeFromTcGblEnv :: GhcReader ParsedModule m => (TcGblEnv -> s) -> m s
 seeFromTcGblEnv fieldSelector = (return . fieldSelector . tcModuleToTcGblEnv <=< typecheckModule) =<< ask
 
+-- | Utility functions for accessing fields from TcGblEnv
 
 seeDefUses :: GhcReader ParsedModule m => m DefUses
 seeDefUses = seeFromTcGblEnv tcg_dus
@@ -170,3 +175,36 @@ seeAnnotations = seeFromTcGblEnv tcg_anns
 
 seeImports :: GhcReader ParsedModule m => m [LImportDecl GhcRn]
 seeImports = seeFromTcGblEnv tcg_rn_imports
+
+
+substitute :: HsExpr GhcPs -> HsExpr GhcPs
+substitute (HsVar a b)        = undefined
+substitute (HsUnboundVar a b) = undefined
+substitute (HsRecFld a b )     = undefined
+substitute (HsOverLabel a b)        = undefined
+substitute (HsIPVar a b)        = undefined
+substitute (HsOverLit a b )        = undefined
+substitute (HsLit a b )        = undefined
+substitute (HsLam a b )        = undefined
+substitute (HsLamCase a b) = undefined
+substitute (HsApp a b c) = undefined
+substitute (HsAppType a b c) = undefined
+substitute (OpApp a b c d) = undefined
+substitute (OpApp a b c d) = undefined
+substitute (NegApp a b c) = undefined
+substitute (HsPar a b) = undefined
+substitute (SectionL a b c) = undefined
+substitute (SectionR a b c) = undefined
+substitute (ExplicitTuple a b c) = undefined
+substitute (ExplicitSum a b c d) = undefined
+substitute (HsCase a b c) = undefined
+substitute (HsIf a b c d) = undefined
+substitute (HsMultiIf a b) = undefined
+substitute (HsLet a b c) = undefined
+substitute (HsDo a b c) = undefined
+substitute (ExplicitList a b) = undefined
+substitute (RecordCon a b c) = undefined
+substitute (RecordUpd a b c) = undefined
+substitute (HsGetField a b c) = undefined
+substitute (HsProjection a b) = undefined
+substitute _ = undefined
